bde7d93eab1a3ca11a0077fe6aa25f1dd4b96a4f
Pürimayr Sarah <PuSh@stiwa.com>
2020-02-20 15:08:48 +0100

    added changing day to DayView

--- t/gui/Input_GUI.py
+++ t/gui/Input_GUI.py
@@ -5,33 +5,58 @@ from Search_code import FoodSearch as Search, Searchterm, FoodBasic
 from Fonts import Fonts
 from clsDay import Day
 import locale
-from datetime import date as Date
+from datetime import date as Date, timedelta
         
 class Dayview(Frame):
+    '''
+    Todo's:
+    * auslagern in eigenes cls-File
+    * Tag mit Scrollbar versehen
+    * food_drop_event
+    '''
+    
     def __init__(self, parent, date):
         super().__init__(parent)
-        self.date=date
+        self._date=date
+        self._today = Day(self._date)
         locale.setlocale(locale.LC_ALL,"")
 
-        self.grid()
+        #self.grid()
 
         self.dayframe = Frame(self,background=Fonts.action_bg)
         self.dayframe.grid(column=0,row=0, padx=Fonts.framedistance, pady=Fonts.framedistance)
-        
-        self.heading = Label(self.dayframe, text=self.date.strftime('%A %d. %B %Y'),font=Fonts.hel_11_b)
-        self.heading.grid(row=0,column=0,padx=Fonts.table_cell_distance,pady=(Fonts.table_cell_distance,0),sticky='nsew') #columnspan
 
+        # headline
+        self.headline=Frame(self.dayframe,)
+        self.heading = Label(self.headline, text='',font=Fonts.hel_11_b)
+        self.btn_previous=Button(self.headline, text='<',command=self.previous_day,relief='flat')
+        self.btn_next=Button(self.headline,text='>',command=self.next_day,relief='flat')
+
+        self.headline.grid(row=0,column=0,padx=Fonts.table_cell_distance,pady=(Fonts.table_cell_distance,0),sticky='nsew') #columnspan
+        self.heading.grid(row=0,column=1,sticky='nsew')
+        self.btn_previous.grid(row=0,column=0)
+        self.btn_next.grid(row=0,column=2)
+        self.headline.columnconfigure(1,weight=1)
+
+        # table
         self.table_entries=Frame(self.dayframe,background=Fonts.table_bg)
-        self.table_entries.grid(row=1,column=0,padx=Fonts.table_cell_distance,pady=(0,Fonts.table_cell_distance))
+        self.table_entries.grid(row=1,column=0,padx=Fonts.table_cell_distance,pady=(0,Fonts.table_cell_distance),sticky='nsew')
 
         header=['Name', 'Menge', 'kCal']
         for col,columnheader in enumerate(header):
             temp_label = Label(self.table_entries, text=columnheader, font=Fonts.hel_8_b) #später eigene Klasse, um Filtern zu ermöglichen
             temp_label.grid(column=col, row=0, padx=1,pady=1,sticky='nswe')
 
-        today = Day(self.date)
+        self.update_day_view()
 
-        for row, entry in enumerate(today.food_entries):
+    def update_day_view(self):
+        self.heading['text'] = self._date.strftime('%A %d. %B %Y')
+        self._today=Day(self._date)
+        self.delete_former_food_entries()
+        self.draw_food_entries()
+
+    def draw_food_entries(self):
+        for row, entry in enumerate(self._today.food_entries):
             temp_row=[]
             temp_row.append(Label(self.table_entries, text=entry.name, anchor='w'))
             temp_row.append(Label(self.table_entries, text=entry.amount))
@@ -39,9 +64,24 @@ class Dayview(Frame):
             for col, widget in enumerate(temp_row):
                 widget.grid(column=col, row=row+1,padx=1,pady=1,sticky='nswe')
                 widget.bind('<ButtonRelease-1>',self.food_dropped)
+        self.table_entries.grid_columnconfigure(1,weight=1)
+        self.table_entries.grid_columnconfigure(2,weight=1)
 
     def food_dropped(self,event):
         self['cursor']='heart'
+
+    def next_day(self):
+        self._date+=timedelta(days=1)
+        self.update_day_view()
+
+    def previous_day(self):
+        self._date-=timedelta(days=1)
+        self.update_day_view()
+
+    def delete_former_food_entries(self):
+        for element in self.table_entries.grid_slaves():
+            if int(element.grid_info()["row"]) > 0:
+                element.grid_forget()
 
 '''
 Stand Input_Window:
@@ -95,7 +135,7 @@ class Input_Window(Frame):
         self.choose_day.grid(column=3, row=0, padx=Fonts.framedistance, pady=0, sticky='w')
 
         # found_food
-        self.results=Frame(self, background='blue')
+        self.results=Frame(self)#, background='blue')
         self.canvas=Canvas(self.results, background='green')
         self.scrollbar = ttk.Scrollbar(self.results, orient='vertical', command=self.canvas.yview)
 
